96c3c28e12383d7b8ffb916753e79e797c5e0764
shingo fukuda <shinshinmice@gmail.com>
2023-01-07 03:08:09 +0900

    書き込みGPIOテスト済み+usart,spiテスト中

---
+++ half_mouse2/Core/Src/PL_gyro.c
@@ -0,0 +1,172 @@
+/*
+ * PL_gyro.c
+ *
+ *  Created on: Jan 5, 2023
+ *      Author: sf199
+ */
+
+
+#include "PL_gyro.h"
+#include "spi.h"
+#include <stdio.h>
+
+
+static uint8_t set_flag = 0;
+
+GYRO_DATA gyro;
+
+uint8_t gyro_read_byte(uint8_t reg)
+
+{
+
+	uint8_t ret, val;
+
+	HAL_GPIO_WritePin( GYRO_CS_GPIO_Port, GYRO_CS_Pin, GPIO_PIN_RESET); //cs = 0;
+
+	ret = reg | 0x80;//先頭のbitを1に
+
+	HAL_SPI_Transmit(&hspi1, &ret, 1, 100);
+
+	HAL_SPI_Receive(&hspi1, &val, 1, 100);
+
+	HAL_GPIO_WritePin( GYRO_CS_GPIO_Port, GYRO_CS_Pin, GPIO_PIN_SET); //cs = 1;
+
+	return val;
+
+}
+
+void gyro_write_byte(uint8_t reg, uint8_t val)
+
+{
+
+	uint8_t ret;
+
+	ret = reg & 0x7F;//先頭のbitを0に
+
+	HAL_GPIO_WritePin( GYRO_CS_GPIO_Port, GYRO_CS_Pin, GPIO_PIN_RESET);
+
+	HAL_SPI_Transmit(&hspi1, &ret, 1, 100);
+
+	HAL_SPI_Transmit(&hspi1, &val, 1, 100);
+
+	HAL_GPIO_WritePin( GYRO_CS_GPIO_Port, GYRO_CS_Pin, GPIO_PIN_SET);
+
+}
+
+void pl_gyro_init(void)
+
+{
+
+	uint8_t who_am_i = 0;
+
+// check WHO_AM_I (0x75)
+
+	who_am_i = gyro_read_byte(0x75);
+
+// who am i = 0x12
+
+	printf("\r\nwho_am_i = 0x%x\r\n", who_am_i);
+
+// recheck
+
+	if (who_am_i != 0x98) {
+
+		HAL_Delay(100);
+
+		who_am_i = gyro_read_byte(0x75);
+
+		if (who_am_i != 0x98) {
+
+			while (1) {
+
+				printf("gyro_error\r");
+
+			}
+
+		}
+
+	}
+
+// set pwr might
+
+// PWR_MIGHT_1 0x6B
+
+	gyro_write_byte(0x6B, 0x00);
+
+	HAL_Delay(50);
+
+// PWR_MIGHT_2 0x6C
+
+	gyro_write_byte(0x6C, 0x00);
+
+	HAL_Delay(50);
+
+// set gyro config
+
+// GYRO_CONFIG 0x1B
+
+	gyro_write_byte(0x1B, 0x18); // use 2000 dps
+
+	HAL_Delay(50);
+
+// ACCEL_CONFIG 0x1C
+
+	gyro_write_byte(0x1C, 0x18); // use pm 16g
+
+	HAL_Delay(50);
+
+	set_flag = 1;
+
+}
+
+float ICM20602_GYRO_READ(uint8_t H_reg)
+
+{
+
+	int16_t data = (int16_t) (((uint8_t) gyro_read_byte(H_reg) << 8)
+			| (uint8_t) gyro_read_byte(H_reg + 1));
+
+	float omega = (float) (data / 16.4f);
+
+	return omega;
+
+}
+
+float ICM20602_ACCEL_READ(uint8_t H_reg)
+
+{
+
+	int16_t data = (int16_t) (((uint8_t) gyro_read_byte(H_reg) << 8)
+			| (uint8_t) gyro_read_byte(H_reg + 1));
+
+	float accel = (float) (data / 2048.0f);
+
+	return accel;
+
+}
+
+void ICM20602_DataUpdate(void)
+
+{
+
+	if (set_flag == 1) {
+
+// get yawrate
+
+		gyro.omega_x = ICM20602_GYRO_READ(0x43);
+
+		gyro.omega_y = ICM20602_GYRO_READ(0x45);
+
+		gyro.omega_z = ICM20602_GYRO_READ(0x47);
+
+// get accel
+
+		gyro.accel_x = ICM20602_ACCEL_READ(0x3B);
+
+		gyro.accel_y = ICM20602_ACCEL_READ(0x3D);
+
+		gyro.accel_z = ICM20602_ACCEL_READ(0x3F);
+
+	}
+
+}
---
+++ half_mouse2/Debug/Core/Startup/startup_stm32g491ceux.d
@@ -0,0 +1,2 @@
+Core/Startup/startup_stm32g491ceux.o: \
+ ../Core/Startup/startup_stm32g491ceux.s
